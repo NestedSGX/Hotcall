b24d68131b438bbbbd74c604920e75fae88427b4
Unik-lif <songlinke18@mails.ucas.ac.cn>
2024-10-10 15:33:06 +0800

    ok

--- occlum_hotcall/src/libos/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!(r"cargo:rustc-link-search=/opt/intel/sgxsdk/lib64");
-}
--- occlum_hotcall/src/pal/src/ocalls/ocalls.h
+++
@@ -1,8 +0,0 @@
-#include "Enclave_u.h"
-#include "../pal_enclave.h"
-#include "../pal_error.h"
-#include "../pal_syscall.h"
-#include "../pal_log.h"
-
-#include "../hotcalls/common.h"
-#include "../hotcalls/hot_calls.h"
